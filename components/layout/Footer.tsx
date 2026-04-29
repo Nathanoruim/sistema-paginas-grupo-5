5ae877d1319187be6587e59df2841ae4aa3bc045
HeitorGonc723 <0001208446@senaimgaluno.com.br>
2026-04-28 21:58:25 -0300

    chore: adiciona meu nome no Footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -9,7 +9,7 @@
  */
 const equipe: { nome: string; github: string }[] = [
   // ====== INÍCIO DA ÁREA DE EDIÇÃO COLABORATIVA ======
-
+{ nome: "Heitor Gonçalves Pereira", github: "HeitorGonc723"},
   // ====== FIM DA ÁREA DE EDIÇÃO COLABORATIVA ======
 ];
 
